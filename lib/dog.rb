3afb9b544b08c544deb1f2307a09d37c073fc14c
spethani <sarahpethani@gmail.com>
2018-08-10 14:22:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 # dog.rb
 class Dog
   def initialize
-    @name
+    @this_dogs_name
   end
   
   def name
-    @name
+    @this_dogs_name
   end
   def name=(dog_name)
-    @name=dog_name
+    @this_dogs_name=dog_name
   end
 end
